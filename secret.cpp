74f5f92db03171a3a50c3aabf5577eab66567a4b
hankli22 <1620110629@qq.com>
2026-01-18 10:20:11 +0800

    Add files via upload

---
+++ secret.cpp
@@ -0,0 +1,5 @@
+#define YOUR_HYPIXEL_APIKEY "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee"
+#define YOUR_MINECRAFT_UUID "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee"
+#define YOUR_WIFI_NAME "aaaaaaaa"
+#define YOUR_WIFI_PASSWORD "aaaaaaaa"
+// Replace the above values with your own.
